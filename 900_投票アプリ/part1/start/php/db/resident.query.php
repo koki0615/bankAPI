c0eac57752ea74fb2771da42f1ee330500d6f26e
koki0615 <onioni0615@gmail.com>
2022-07-15 18:56:03 +0900

    マンション

---
+++ 900_投票アプリ/part1/start/php/db/resident.query.php
@@ -0,0 +1,58 @@
+<?php 
+namespace db;
+
+use db\DataSource;
+use model\ResidentModel;
+
+class ResidentQuery {
+    //データベースからidをもとに値を取得する関数
+    public static function fetchResidentId() {
+
+        
+
+        $db = new DataSource;
+        $sql = 'select * from mansion;';
+
+        $result = $db->select($sql, [
+            
+        ], DataSource::CLS, MansionModel::class);
+
+        return $result;
+
+    }
+    
+    public static function fetchById($mansion) {
+
+        
+
+        $db = new DataSource;
+        $sql = '
+        select * from mansion m
+        where m.id = :id';
+
+        $result = $db->selectOne($sql, [
+            ':id' => $mansion->id
+        ], DataSource::CLS, MansionModel::class);
+
+        return $result;
+
+    }
+
+    
+
+    // public static function insert($user) {
+
+    //     $db = new DataSource;
+    //     $sql = 'insert into master(id, pwd) values (:id, :pwd)';
+
+    //     $user->pwd = password_hash($user->pwd, PASSWORD_DEFAULT);
+
+    //     return $db->execute($sql, [
+    //         ':id' => $user->id,
+    //         ':pwd' => $user->pwd
+    //     ]);
+
+    // }
+
+
+}
